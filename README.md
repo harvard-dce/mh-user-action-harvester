9efee4df444f1078444c3e220d426b21a8d2f9b6
Jay Luker <jay_luker@harvard.edu>
2016-09-14 15:50:19 -0400

    new command for loading mh episode data directly into elasticsearch

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # mh-user-action-harvester
+
+A python script for fetching useraction events and related metadata from Opencast Matterhorn. The script uses the [pyhorn](https://github.com/harvard-dce/pyhorn) library for interacting with the Matterhorn API. Harvested useractions are fed to an SQS queue.
+
+## Setup
+
+1. clone this repo and run `pip install -r requiements.txt`
+1. copy `example.env` to `.env` and update the env var settings
+3. run `./ua_harvest.py --help` for commands and usage
+
+## Commands
+
+The script uses the [click](http://click.pocoo.org/) library to implement its CLI. There are two sub-commands, `harvest` and `load_episodes`, both of which take the `--help` option to display usage.
+
+### `harvest`
+
+This command fetches batches of useraction events based on a `--start` and `--end` timestamp. If a start/end is not specified the script will look for and use the timestamp of the last useraction fetched (stored in an S3 bucket; see settings below) as the start value and `now()` as the end value. If no timestamp is stored in S3 the default is to fetch the last `--interval` minutes of events (defaults to 2 minutes). Events are fetched in batches of `--batch-size` (default 1000) using the API endpoint's `limit` and `offset` parameters. Events are output to an SQS queue identified with `--queue-name`. If `--queue-name` is `"-"` the json data will be sent to stdout.
+
+There is one additional option, `--disable-start-end-span-check`, that prevents the harvester's start/end timestamps from growing too large. See details below.
+
+### `load_episodes`
+
+This command fetches episode metadata from the Matterhorn search endpoint. By default it looks for episodes that have been created in the past 1 day. This can be controlled with the `--created_from_days_ago` option. The episode records are augmented with additional information about live stream start/stop times and availability via the admin node's workflow API endpoint. The resulting records are sent to an Elasticsearch index, identified by `--es_host` and `--es_index`.
+
+## Settings
+
+A local `.env` file will be read automatically and is the preferred way of passing options to the commands. 
+
+#### MATTERHORN_REST_USER / MATTERHORN_REST_PASS
+The user/pass combo for accessing the API.
+
+#### MATTERHORN_ENGAGE_HOST
+IP or hostname of the engage node.
+
+#### MATTERHORN_ADMIN_HOST
+IP or hostname of the admin node.
+
+#### DEFAULT_INTERVAL
+If no `--start` is provided and no last action timestamp is found in s3 the useraction harvest start will be calculated as this many minutes ago.
+
+#### LOGGLY_TOKEN
+If present the script will send log events to loggly.
+
+#### LOGGLY_TAGS
+Any extra tags to attach to events sent to loggly.
+
+#### S3_HARVEST_TS_BUCKET
+Name of the S3 bucket to store the last action's timestamp value.
+
+#### S3_LAST_ACTION_TS_KEY
+Name of the S3 object the last action timestamp is saved as.
+
+#### SQS_QUEUE_NAME
+Name of the SQS queue to send useraction events. The queue will be created if it doesn't exist.
+
+#### ELASTICSEARCH_HOST
+Hostname or IP of the Elasticsearch instance in which to index the episode records.
+
+## useraction harvest too-big timespan protection
+
+The `harvest` command has a built-in protection against the start/end time range growing too large. This can happen if the harvester for some reason or other falls behind in fetching the most recent useraction events. For instance, the script fails for some reason or the engage node becomes unresponsive for some length of time. Because the last action timestamp value is only updated on a successful harvest run, the time span the harvester wants to fetch could grow so large that the harvesting process becomes bogged down. To protect against this the harvester will abort if this time span is longer than 3600 seconds (1 hour). If this situation arises there are two courses of action:
+
+1. Manually run the harvester using the `--disable-start-end-span-check` flag. It is recommended that you also ensure no other harvester processes run concurrently by either disabling any cron jobs or using something like `/bin/run-one`.
+2. Reset the start time by deleting the S3 object, `s3://<S3_HARVEST_TS_BUCKET>/<S3_LAST_ACTION_TS_KEY`. You'll then need to manually harvest the useraction events that were missed. If the gap is large you can do several runs manipulating the `--start/--end` range as necessary.
--- ua_harvest.py
+++ ua_harvest.py
@@ -10,6 +10,9 @@ import pyloggly
 from os import getenv
 from os.path import dirname, join
 
+import re
+from elasticsearch import Elasticsearch
+
 import pyhorn
 # force all calls to the episode search endpoint to use includeDeleted=true
 pyhorn.endpoints.search.SearchEndpoint._kwarg_map['episode']['includeDeleted'] = True
@@ -20,22 +23,16 @@ from botocore.exceptions import ClientError
 from dotenv import load_dotenv
 load_dotenv(join(dirname(__file__), '.env'))
 
-MATTERHORN_REST_USER = getenv('MATTERHORN_REST_USER')
-MATTERHORN_REST_PASS = getenv('MATTERHORN_REST_PASS')
-MATTERHORN_HOST = getenv('MATTERHORN_HOST')
-DEFAULT_INTERVAL = getenv('DEFAULT_INTERVAL', 2)
-LOGGLY_TOKEN = getenv('LOGGLY_TOKEN')
-LOGGLY_TAGS = getenv('LOGGLY_TAGS')
-S3_LAST_ACTION_TS_BUCKET = getenv('S3_LAST_ACTION_TS_BUCKET', 'mh-user-action-harvester')
-S3_LAST_ACTION_TS_KEY = getenv('S3_LAST_ACTION_TS_KEY')
-SQS_QUEUE_NAME = getenv('SQS_QUEUE_NAME')
 MAX_START_END_SPAN_SECONDS = 3600
 
 log = logging.getLogger('mh-user-action-harvester')
-log.setLevel(logging.INFO)
+log.setLevel(logging.DEBUG)
 console = logging.StreamHandler(stream=sys.stdout)
 console.setFormatter(logging.Formatter("%(name)s %(levelname)s %(message)s"))
 log.addHandler(console)
+
+LOGGLY_TOKEN = getenv('LOGGLY_TOKEN')
+LOGGLY_TAGS = getenv('LOGGLY_TAGS')
 if LOGGLY_TOKEN is not None:
     log.addHandler(
         pyloggly.LogglyHandler(
@@ -48,31 +45,35 @@ if LOGGLY_TOKEN is not None:
 sqs = boto3.resource('sqs')
 s3 = boto3.resource('s3')
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+@cli.command()
 @click.option('-s', '--start', help='YYYYMMDDHHmmss')
 @click.option('-e', '--end', help='YYYYMMDDHHmmss; default=now')
 @click.option('-w', '--wait', default=1,
               help="Seconds to wait between batch requests")
-@click.option('-H', '--hostname', default=MATTERHORN_HOST,
-              help="Matterhorn engage hostname")
-@click.option('-u', '--user', default=MATTERHORN_REST_USER,
-              help='Matterhorn rest user')
-@click.option('-p', '--password', default=MATTERHORN_REST_PASS,
-              help='Matterhorn rest password')
+@click.option('-H', '--engage_host', envvar='MATTERHORN_ENGAGE_HOST',
+              help="Matterhorn engage hostname", required=True)
+@click.option('-u', '--user', envvar='MATTERHORN_REST_USER',
+              help='Matterhorn rest user', required=True)
+@click.option('-p', '--password', envvar='MATTERHORN_REST_PASS',
+              help='Matterhorn rest password', required=True)
 @click.option('-o', '--output', default='sqs',
               help='where to send output. use "-" for json/stdout')
-@click.option('-q', '--queue-name', default=SQS_QUEUE_NAME,
-              help='SQS queue name')
+@click.option('-q', '--queue-name', envvar='SQS_QUEUE_NAME',
+              help='SQS queue name', required=True)
 @click.option('-b', '--batch-size', default=1000,
               help='number of actions per request')
-@click.option('-i', '--interval', default=DEFAULT_INTERVAL,
+@click.option('-i', '--interval', envvar='DEFAULT_INTERVAL', default=2,
               help='Harvest action from this many mintues ago')
 @click.option('--disable-start-end-span-check', is_flag=True,
               help="Don't abort on too-long start-end time spans")
-def harvest(start, end, wait, hostname, user, password, output, queue_name,
+def harvest(start, end, wait, engage_host, user, password, output, queue_name,
             batch_size, interval, disable_start_end_span_check):
 
-    mh = pyhorn.MHClient('http://' + hostname, user, password, timeout=30)
+    mh = pyhorn.MHClient('http://' + engage_host, user, password, timeout=30)
 
     if output == 'sqs':
         queue = get_or_create_queue(queue_name)
@@ -80,8 +81,9 @@ def harvest(start, end, wait, hostname, user, password, output, queue_name,
     if end is None:
         end = arrow.now().format('YYYYMMDDHHmmss')
 
+    last_action_ts_key = getenv('S3_LAST_ACTION_TS_KEY')
     if start is None:
-        start = get_last_action_ts()
+        start = get_harvest_ts(last_action_ts_key)
         if start is None:
             start = arrow.now() \
                 .replace(minutes=-interval) \
@@ -159,7 +161,7 @@ def harvest(start, end, wait, hostname, user, password, output, queue_name,
             last_action_ts = end
         else:
             last_action_ts = arrow.get(last_action.created).format('YYYYMMDDHHmmss')
-        set_last_action_ts(last_action_ts)
+        set_harvest_ts(last_action_ts_key, last_action_ts)
         log.info("Setting last action timestamp to %s", last_action_ts)
     except Exception, e:
         log.error("Failed setting last action timestamp: %s", str(e))
@@ -225,27 +227,175 @@ def create_action_rec(action):
 
     return rec
 
+@cli.command()
+@click.option('-c', '--created_from_days_ago', type=int, default=1,
+              help='set createdFrom param to this many days ago')
+@click.option('-A', '--admin_host', envvar='MATTERHORN_ADMIN_HOST',
+              help="Matterhorn admin hostname", required=True)
+@click.option('-E', '--engage_host', envvar='MATTERHORN_ENGAGE_HOST',
+              help="Matterhorn engage hostname", required=True)
+@click.option('-u', '--user', envvar='MATTERHORN_REST_USER',
+              help='Matterhorn rest user', required=True)
+@click.option('-p', '--password', envvar='MATTERHORN_REST_PASS',
+              help='Matterhorn rest password', required=True)
+@click.option('-e', '--es_host', envvar='ELASTICSEARCH_HOST',
+              help="Elasticsearch host:port", default='localhost:9200')
+@click.option('-i', '--es_index', envvar='EPISODE_INDEX_NAME', default='episodes',
+              help="Name of the index for storing episode records")
+@click.option('-w', '--wait', default=1,
+              help="Seconds to wait between batch requests")
+def load_episodes(created_from_days_ago, admin_host, engage_host, user, password, es_host,
+                  es_index, wait):
+
+    mh_admin = pyhorn.MHClient('http://' + admin_host, user, password, timeout=30)
+    mh_engage = pyhorn.MHClient('http://' + engage_host, user, password, timeout=30)
+    es = Elasticsearch([es_host])
+
+    offset = 0
+    episode_count = 0
+    batch_size = 100
+
+    while True:
+        request_params = {
+            'offset': offset,
+            'limit': batch_size,
+            'includeDeleted': True,
+            'sort': 'DATE_CREATED'
+        }
+
+        if created_from_days_ago is not None:
+            request_params['createdFrom'] = arrow.now() \
+                .replace(days=-created_from_days_ago).format('YYYY-MM-DDTHH:mm:ss') + 'Z'
+
+        episodes = mh_engage.search_episodes(**request_params)
+        if len(episodes) == 0:
+            log.info("no more episodes")
+            break
+
+        episode_count += len(episodes)
+        log.info("fetched %d total episodes", episode_count)
+
+        for ep in episodes:
+            doc = {
+                'title': ep.mediapackage.title,
+                'mpid': ep.mediapackage.id,
+                'duration': int(ep.mediapackage.duration),
+                'start': str(arrow.get(ep.mediapackage.start).to('utc'))
+            }
+
+            try:
+                series = str(ep.mediapackage.series)
+                doc.update({
+                    'course': ep.mediapackage.seriestitle,
+                    'series': series,
+                    'year': series[:4],
+                    'term': series[4:6],
+                    'crn': series[6:11]
+                })
+            except AttributeError:
+                series = None
+                log.warn("Missing series for episode %s", ep.id)
+
+            try:
+                doc['type'] = ep.dcType
+            except AttributeError:
+                log.warn("Missing type for episode %s", ep.id)
+
+            try:
+                doc['description'] = ep.dcDescription
+            except AttributeError:
+                log.warn("Missing description for episode %s", ep.id)
+
+            attachments = ep.mediapackage.attachments
+            if isinstance(attachments, dict):
+                try:
+                    attachments = attachments['attachment']
+
+                    for preview_type in ['presenter', 'presentation']:
+                        try:
+                            preview = next(a for a in attachments if a['type'] == '%s/player+preview' % preview_type)
+                            doc['%s_still' % preview_type] = preview['url']
+                        except StopIteration:
+                            pass
+
+                    doc['slides'] = [{
+                        'img': a['url'],
+                        'time': re.search('time=([^;]+)', a['ref']).group(1)
+                    } for a in attachments if a['type'] == 'presentation/segment+preview']
+
+                except Exception, e:
+                    log.error("Failed to extract attachement info from episode %s: %s", ep.id, str(e))
+
+            try:
+                wfs = mh_admin.workflows(
+                    mp=ep.mediapackage.id,
+                    state='SUCCEEDED',
+                    workflowdefinition='DCE-archive-publish-external'
+                    )
+
+                if len(wfs) > 1:
+                    raise RuntimeError("Got > 1 workflow for mpid %s" % ep.mediapackage.id)
+                else:
+                    wf = wfs[0]
+
+                ops = wf.operations
+
+                try:
+                    capture = next(x for x in ops if x.id == 'capture')
+                    doc.update({
+                        'live_stream': 1,
+                        'live_start': str(arrow.get(capture.started / 1000)),
+                        'live_end': str(arrow.get(capture.completed / 1000)),
+                        'live_duration': capture.completed - capture.started
+                    })
+                except StopIteration:
+                    doc['live_stream'] = 0
+
+                try:
+                    retract = next(x for x in ops if x.id == 'retract-element')
+                    doc['available'] = str(arrow.get(retract.completed / 1000))
+                except StopIteration:
+                    pass
+
+            except IndexError:
+                log.info("No matching or finished workflow found for %s: %s", ep.id, ep.mediapackage.title)
+            except Exception, e:
+                log.error("Failed extracting workflow data for episode %s: %s", ep.id, str(e))
+
+            es.index(index=es_index,
+                     doc_type='episode',
+                     id=ep.id,
+                     body=doc
+                     )
+
+        time.sleep(wait)
+        offset += batch_size
 
 # s3 state bucket helpers
-def set_last_action_ts(last_action_ts):
+def set_harvest_ts(ts_key, timestamp):
     bucket = get_or_create_bucket()
-    bucket.put_object(Key=S3_LAST_ACTION_TS_KEY, Body=last_action_ts)
+    bucket.put_object(Key=ts_key, Body=timestamp)
 
-def get_last_action_ts():
+def get_harvest_ts(ts_key):
     bucket = get_or_create_bucket()
     try:
-        obj = bucket.Object(S3_LAST_ACTION_TS_KEY).get()
+        obj = bucket.Object(ts_key).get()
         return obj['Body'].read()
     except ClientError:
-        log.debug("No last_update value found")
+        log.debug("No %s value found", ts_key)
         return None
 
 def get_or_create_bucket():
+
+    bucket_name = getenv('S3_HARVEST_TS_BUCKET')
+    if bucket_name is None:
+        raise RuntimeError("No timestamp bucket specified!")
+
     try:
-        s3.meta.client.head_bucket(Bucket=S3_LAST_ACTION_TS_BUCKET)
-        return s3.Bucket(S3_LAST_ACTION_TS_BUCKET)
+        s3.meta.client.head_bucket(Bucket=bucket_name)
+        return s3.Bucket(bucket_name)
     except ClientError:
-        return s3.create_bucket(Bucket=S3_LAST_ACTION_TS_BUCKET)
+        return s3.create_bucket(Bucket=bucket_name)
 
 def get_or_create_queue(queue_name):
     try:
@@ -255,5 +405,4 @@ def get_or_create_queue(queue_name):
 
 
 if __name__ == '__main__':
-    harvest()
-
+    cli()
